b96cec433c6c3d255b75422d716015d5ebc405f5
Will Sewell <willsewell@echo.xyz>
2025-09-25 19:10:18 +0100

    Basic example of authenticating with sonar and fetching entity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
+        <Provider>
+            {children}
+        </Provider>
+        </div>
       </body>
     </html>
   );
---
+++ src/app/oauth/callback/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { useSonarAuth } from "@echoxyz/sonar-react";
+import { useRouter } from "next/navigation";
+
+export default function OAuthCallback() {
+    const router = useRouter();
+    const { authenticated, completeOAuth, ready } = useSonarAuth();
+    const oauthCompletionTriggered = useRef(false);
+    const [oauthError, setOAuthError] = useState<string | null>(null);
+    const [timedOut, setTimedOut] = useState(false);
+
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+    const state = params.get("state");
+
+    // complete the oauth flow and exchange the code for an access token
+    useEffect(() => {
+        const processOAuthCallback = async () => {
+            // the user is already authenticated, nothing to do
+            if (!ready || authenticated || !code || !state) {
+                return;
+            }
+
+            // ensuring the oauth completion isn't called multiple times since subsequent ones are expected to fail
+            if (oauthCompletionTriggered.current) {
+                return;
+            }
+            oauthCompletionTriggered.current = true;
+
+            try {
+                await completeOAuth({ code, state });
+            } catch (err) {
+                setOAuthError(err instanceof Error ? err.message : null);
+            }
+        };
+
+        processOAuthCallback();
+    }, [authenticated, completeOAuth, code, ready, state]);
+
+    // fetch the user's available entities after they've been authenticated
+    useEffect(() => {
+        if (!ready || !authenticated) {
+            return;
+        }
+        router.push("/");
+    }, [authenticated, router, ready]);
+
+    // set a timeout, so we don't keep the user waiting indefinitely in case of an unexpected issue
+    useEffect(() => {
+        setTimedOut(false);
+        const timeoutId = setTimeout(() => setTimedOut(true), 20000);
+        return () => clearTimeout(timeoutId);
+    }, [setTimedOut]);
+
+    if (timedOut) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="text-center">
+                    <h2 className="text-xl font-semibold text-red-600 mb-2">Timed out</h2>
+                    <button
+                        onClick={() => router.push("/")}
+                        className="mt-4 px-4 py-2 bg-gray-50 text-gray-900 rounded-xl cursor-pointer"
+                    >
+                        Return to Login
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    if (oauthError) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <div className="text-center">
+                    <h2 className="text-xl font-semibold text-red-600 mb-2">{oauthError}</h2>
+                    <button
+                        onClick={() => router.push("/")}
+                        className="mt-4 px-4 py-2 bg-gray-50 text-gray-900 rounded-xl cursor-pointer"
+                    >
+                        Return to Login
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="text-center">
+                <h2 className="text-xl font-semibold mb-4 text-white">Connecting to Echo</h2>
+            </div>
+        </div>
+    );
+}
