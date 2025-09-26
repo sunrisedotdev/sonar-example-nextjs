547a24716213df5e174184651e2373ee9af28d5d
Will Demaine <willdemaine@gmail.com>
2025-09-26 17:13:53 +0100

    Improve loading states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,7 @@ const SonarAuthButton = ({
 };
 
 export type ResourceState<T> = {
+  hasFetched: boolean;
   loading: boolean;
   error?: string;
   value?: T;
@@ -45,11 +46,30 @@ export type ResourceState<T> = {
 // support purchase flow, including liveness checks
 
 export default function Home() {
+  const { login, authenticated, logout } = useSonarAuth();
+
+  return (
+    <div className="flex flex-col gap-5 p-5 w-[500px] justify-center items-center">
+      <div className="flex flex-row gap-4">
+        <ConnectKitButton />
+        <SonarAuthButton
+          authenticated={authenticated}
+          login={() => login()}
+          logout={() => logout()}
+        />
+      </div>
+      <SonarEntityPanel />
+    </div>
+  );
+}
+
+const SonarEntityPanel = () => {
   const client = useSonarClient();
-  const { login, authenticated, ready, logout } = useSonarAuth();
+  const { authenticated, ready } = useSonarAuth();
   const { address, isConnected } = useAccount();
 
   const [entity, setEntity] = useState<ResourceState<EntityDetails>>({
+    hasFetched: false,
     loading: false,
     value: undefined,
   });
@@ -65,24 +85,29 @@ export default function Home() {
         walletAddress: address,
       });
       setEntity({
+        hasFetched: true,
         loading: false,
         value: entity.Entity,
       });
     } catch (error) {
       setEntity({
+        hasFetched: true,
         loading: false,
         error: error instanceof Error ? error.message : "Unknown error",
       });
     }
   }, [ready, authenticated, client, address, isConnected]);
 
+  // Read entity on mount
   useEffect(() => {
     readEntity();
   }, [readEntity]);
 
+  // Reset entity state if a user is not authenticated or doesn't have a connected wallet
   useEffect(() => {
     if (ready && (!authenticated || !address || !isConnected)) {
       setEntity({
+        hasFetched: false,
         loading: false,
         value: undefined,
         error: undefined,
@@ -90,20 +115,23 @@ export default function Home() {
     }
   }, [ready, authenticated, address, isConnected]);
 
-  return (
-    <div className="flex flex-col gap-5 p-5 w-[500px] justify-center items-center">
-      <div className="flex flex-row gap-4">
-        <ConnectKitButton />
-        <SonarAuthButton
-          authenticated={authenticated}
-          login={() => login()}
-          logout={() => logout()}
-        />
-      </div>
+  // Coordinate the state of both the wallet and Sonar account being connected.
+  const fullyConnected = ready && authenticated && isConnected;
+  const hasTriedLoading =
+    !entity.loading &&
+    (entity.value !== undefined || entity.error !== undefined);
 
-      {authenticated && (
-        <SonarEntity key={entity.value?.EntityUUID} value={entity.value} />
-      )}
-    </div>
-  );
-}
+  if (!fullyConnected) {
+    return <p>Connect your wallet and Sonar account to continue</p>;
+  }
+
+  if (!ready || entity.loading || !hasTriedLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (entity.error) {
+    return <p>Error: {entity.error}</p>;
+  }
+
+  return <SonarEntity key={entity.value?.EntityUUID} value={entity.value} />;
+};
